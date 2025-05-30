0b8eaf8fdf485eb48a56604ebdf69719ba91d9a6
Antonio <salvalaggio.antonio@gmail.com>
2025-05-30 15:32:10 +0200

    Added comments to python scripts

--- src/python/CreateIndex.py
+++ src/python/CreateIndex.py
@@ -3,15 +3,22 @@ from os.path import isfile
 from re import compile
 from sys import argv
 
+# Input arguments: 
+#  String: input file location (must be a directory path, ending with /, example: ./inputs/)
+#  String: output file
 if len(argv) < 3:
     exit(1)
 
 dir = str(argv[1])
 out = str(argv[2])
 
+# Regex pattern pre-compile
 pattern = compile("[^A-Za-z]+")
+
+# Index data structure
 index = dict()
 
+# Loop through all the files in the input directory
 for file in listdir(path=dir):
 
     if isfile(dir+file):
@@ -19,8 +26,11 @@ for file in listdir(path=dir):
         with open(dir+file, "r") as f:
 
             for x in f:
+
+                # Add each word reference to the index
                 for word in x.split():
                     
+                    # Word cleanup
                     word = pattern.sub('', word)
 
                     if len(word) == 0:
@@ -28,16 +38,20 @@ for file in listdir(path=dir):
 
                     word = word.lower()
 
+                    # If the word is not in the index add it
                     if not word in index:
                         index.update([(word, dict())])
 
+                    # Add the reference to the index:
                     if file in index[word]:
+                        # If the file is already in the list of a given word, increase the count
                         index[word][file] += 1
                     else:
+                        # If the file is not in the list of a given word, add it with count 1
                         index[word].update([(file, 1)])
 
+# Output the index in with a line for each word with tab separeted pairs of all relative <filename>:<count> references
 indexFile=open(out, "w")
-
 for word, list in index.items():
     out = word
     for file, count in list.items():
--- src/python/SearchIndex.py
+++ src/python/SearchIndex.py
@@ -3,34 +3,47 @@ from os.path import isfile
 from re import compile
 from sys import argv
 
+# Input argument: 
+#  String: index location (can be either a file or a folder containing multiple index files)
 if len(argv) < 2:
     exit(1)
 
 dir = str(argv[1])
 
+# Regex pattern pre-compile
 pattern = compile("[^A-Za-z]+")
+
+# Index data structure
 index = dict()
 
-
+# Function to parse an index file and add its contents to the index data structure
 def handleFile(f):
+    # Loop through the file lines
     for x in f:
 
+        # Loop thorugh all the tab separated values of each line
         word = ""
         for val in x.split("\t"):
 
+            # The first value is always the word
             if word == "":
                 word = val
                 if not word in index:
                     index.update([(word, dict())])
                 continue
             
+            # Subsequent values are in the format <filename>:<count>
             val = val.strip("\n")
             val = val.split(":")
+            # Add the values to the index
             if val[0] in index[word]:
+                # If using multiple index files it's possible to have different counts of a word for the same filename
+                # We decided to add them together
                 index[word][val[0]] += int(val[-1])
             else:
                 index[word].update([(val[0], int(val[-1]))]) 
 
+# Parse index file/directory
 if isfile(dir):
     with open(dir, "r") as f:
         handleFile(f)
@@ -40,10 +53,13 @@ else:
             with open(dir+file) as f:
                 handleFile(f)
 
+
 result = set()
 first = True
+# Take from stdin the words to search for
 for word in input().split():
     
+    # Cleanup the words in the same way the index generator does
     word = pattern.sub('', word)
 
     if len(word) == 0:
@@ -51,18 +67,23 @@ for word in input().split():
 
     word = word.lower()
 
+    # If a word is not in the index the final result should be empty, we can stop the search early
     if not word in index:
         result.clear()
         break
 
     if first:
+        # The first word provides a list of possible result filenames
         result = set(index[word].keys())
         first = False
     else:
+        # All other words can remove possible result filenames
         result = result.intersection(index[word].keys())
     
+    # If there are no more possible results we can stop early
     if len(result) == 0:
         break
 
+# Result output
 for r in result:
     print(r)
